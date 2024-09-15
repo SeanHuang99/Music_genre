c5d501a7ca4509859cece807dd2de347607c1259
Shengchun <huangshengchuntc6@gmail.com>
2024-09-16 00:37:38 +0100

    cnn using countvectorizer and mxm_msd_genre.cls

--- scripts/bagofwords/data_preparation01.py
+++ scripts/bagofwords/data_preparation01.py
@@ -2,34 +2,20 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import CountVectorizer
+
 import torch
 from torch.utils.data import DataLoader, TensorDataset
-import re  # 用于提取单词
-
-
-def extract_words_with_count(text):
-    """从 'word(count)' 格式中提取单词并基于 count 重复单词"""
-    if isinstance(text, str):  # Only process if text is a string
-        # 匹配 'word(count)' 格式的单词和计数
-        matches = re.findall(r'(\w+)\((\d+)\)', text)
-        # 将单词重复 'count' 次，形成最终的词汇列表
-        words = [word for word, count in matches for _ in range(int(count))]
-        return ' '.join(words)
-    else:
-        return ''  # Return an empty string if the input is not valid
 
 
 def prepare_data():
     # 读取CSV文件
-    df = pd.read_csv('../data/mxm_msd_genre_pro_no_stopwords.cls')
-
-    # 提取出 `word_with_count` 字段中的单词部分，并根据 count 重复单词
-    df['word'] = df['x'].apply(extract_words_with_count)
+    df = pd.read_csv('.././data/mxm_msd_genre.cls')
 
     # 将词袋形式的单词合并成句子形式
     df_grouped = df.groupby(['trackId', 'genre', 'is_split'])['word'].apply(lambda x: ' '.join(x)).reset_index()
 
     # 使用TfidfVectorizer将文本转化为TF-IDF特征
+    # vectorizer = TfidfVectorizer()
     vectorizer = CountVectorizer()
     X = vectorizer.fit_transform(df_grouped['word'])
 
@@ -56,7 +42,6 @@ def prepare_data():
     test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
     return train_loader, test_loader, len(vectorizer.get_feature_names_out()), len(label_encoder.classes_)
-
 
 # 如果这个文件是直接运行的，那么可以如下执行数据准备
 if __name__ == "__main__":
